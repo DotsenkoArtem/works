fd78857d1afe98ab7de7933f0674e6d099bd4215
DotsenkoArtem <artemdoc1@rambler.ru>
2023-09-26 14:07:48 +0500

    Added pages: Sevices, Gallery

--- foka-doka/public/js/gallery.dev.js
+++ foka-doka/public/js/gallery.dev.js
@@ -6,3 +6,41 @@ Fancybox.bind("[data-fancybox]", {
   },
   transition: 'classic'
 });
+
+// T A B S
+"use strict";
+var tabsBoxes = document.querySelectorAll('.tabs-wrap');
+var _loop = function _loop() {
+  // ПОЛУЧЕНИЕ МАССИВА ВСЕХ ВКЛАДОК
+  var tabs = tabsBoxes[i].querySelectorAll('.tabs__btn');
+  // ПОЛУЧЕНИЕ БЛОКА С ЭЛЕМЕНТАМИ КОНТЕНТА ВКЛАДОК
+  var tabsContent = tabsBoxes[i].querySelector('.tabs-content');
+  // ПОЛУЧЕНИЕ ВСЕХ ЭЛЕМЕНТОВ КОНТЕНТА ВКЛАДОК
+  var tabsContentItems = tabsContent.querySelectorAll('.tabs-content__item');
+  for (var _i = 0; _i < tabs.length; _i++) {
+    var tab = tabs[_i];
+    tab.addEventListener('click', function () {
+      // УБАРИЕМ КЛАСС "ACTIVE" СО ВСЕХ ВКЛАДОК
+      for (var j = 0; j < tabs.length; j++) {
+        tabs[j].classList.remove('active');
+      }
+
+      // АКТИВНАЯ ВКЛАДКА ПРИ КЛИКЕ
+      var tabContentId = '#' + this.dataset.target;
+      this.classList.add('active');
+
+      // УБАРИЕМ КЛАСС "ACTIVE" СО ВСЕХ ЭЛЕМЕНТОВ КОНТЕНТА ВКЛАДОК
+      for (var k = 0; k < tabsContentItems.length; k++) {
+        tabsContentItems[k].classList.remove('active');
+      }
+
+      // АКТИВНЫЙ ЭЛЕМЕНТ КОНТЕНА ВКЛАДКИ ПРИ КЛИКЕ
+      tabsContent.querySelector(tabContentId).classList.add('active');
+    });
+  }
+};
+for (var i = 0; i < tabsBoxes.length; i++) {
+  _loop();
+}
+
+// ==============================================================================================================
--- foka-doka/public/js/gallery.js
+++ foka-doka/public/js/gallery.js
@@ -6,3 +6,45 @@ Fancybox.bind("[data-fancybox]", {
     transition: 'classic',
   
 });
+
+
+// T A B S
+"use strict"
+
+const tabsBoxes = document.querySelectorAll('.tabs-wrap');
+
+for (let i = 0; i < tabsBoxes.length; i++) {
+	// ПОЛУЧЕНИЕ МАССИВА ВСЕХ ВКЛАДОК
+	const tabs = tabsBoxes[i].querySelectorAll('.tabs__btn');
+	// ПОЛУЧЕНИЕ БЛОКА С ЭЛЕМЕНТАМИ КОНТЕНТА ВКЛАДОК
+	const tabsContent = tabsBoxes[i].querySelector('.tabs-content');
+	// ПОЛУЧЕНИЕ ВСЕХ ЭЛЕМЕНТОВ КОНТЕНТА ВКЛАДОК
+	const tabsContentItems = tabsContent.querySelectorAll('.tabs-content__item');
+
+
+	for (let i = 0; i < tabs.length; i++) {
+		let tab = tabs[i];
+
+		tab.addEventListener('click', function () {
+			// УБАРИЕМ КЛАСС "ACTIVE" СО ВСЕХ ВКЛАДОК
+			for (let j = 0; j < tabs.length; j++) {
+				tabs[j].classList.remove('active');
+			}
+
+
+			// АКТИВНАЯ ВКЛАДКА ПРИ КЛИКЕ
+			let tabContentId = '#' + this.dataset.target;
+			this.classList.add('active');
+
+			// УБАРИЕМ КЛАСС "ACTIVE" СО ВСЕХ ЭЛЕМЕНТОВ КОНТЕНТА ВКЛАДОК
+			for (let k = 0; k < tabsContentItems.length; k++) {
+				tabsContentItems[k].classList.remove('active');
+			}
+
+			// АКТИВНЫЙ ЭЛЕМЕНТ КОНТЕНА ВКЛАДКИ ПРИ КЛИКЕ
+			tabsContent.querySelector(tabContentId).classList.add('active');
+		})
+	}
+}
+
+// ==============================================================================================================
